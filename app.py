09b4a4810d2d1b09eb76dc2720debb6abd141a3e
Blaze34536 <88854346+Blaze34536@users.noreply.github.com>
2025-12-25 11:57:36 -0600

    feat: frontend implementation v1

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("rfqEditor.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login")
+def logout():
+    return render_template("logout.html")
+
+@app.route("/rfq-list")
+def rfqList():
+    return render_template("rfqList.html")
+
+# @app.route("/rfq-editor")
+# def rfqList():
+#     return render_template("rfqEditor.html")
