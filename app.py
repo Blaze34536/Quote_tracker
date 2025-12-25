ba022e82e30d72a8f0ef436e5bace2bc4c648a7a
Blaze34536 <88854346+Blaze34536@users.noreply.github.com>
2025-12-25 12:46:37 -0600

    feat: added login and signup frontend

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("rfqEditor.html")
+    return render_template("rfqEditor2.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route("/login")
+@app.route("/signup")
+def signin():
+    return render_template("signup.html")
+
+@app.route("/logout")
 def logout():
     return render_template("logout.html")
 
